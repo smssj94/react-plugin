2241c197f31cc126aa654c5afecc8da892e03c00
Jiyoung Hwang <jiyoung.hwang@takeaway.com>
2021-05-29 11:39:29 +0200

    Clean up

--- code.js
+++ code.js
@@ -1,1 +1,24 @@
-throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
+import { icons } from 'feather-icons';
+figma.showUI(__html__);
+figma.ui.onmessage = msg => {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    // if (msg.type === 'create-rectangles') {
+    //   const nodes: SceneNode[] = [];
+    //   for (let i = 0; i < msg.count; i++) {
+    //     const rect = figma.createRectangle();
+    //     rect.x = i * 150;
+    //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    //     figma.currentPage.appendChild(rect);
+    //     nodes.push(rect);
+    //   }
+    //   figma.currentPage.selection = nodes;
+    //   figma.viewport.scrollAndZoomIntoView(nodes);
+    // }
+    const icon = figma.createNodeFromSvg(icons['heart'].toSvg());
+    icon.name = msg.type;
+    icon.x = figma.viewport.center.x;
+    icon.y = figma.viewport.center.y;
+    figma.currentPage.selection = [icon];
+    // figma.closePlugin();
+};
